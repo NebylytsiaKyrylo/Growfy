7f89f584b54b7459a044eda7ab500f65c81e2c3f
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2023-05-02 17:52:32 +0200

    Quatrième partie

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ document.addEventListener('click', documentClick);
 
 function documentClick(e) {
   const targetItem = e.target;
-  console.log(targetItem);
   if (targetItem.closest('.icon-menu')) {
     document.documentElement.classList.toggle('menu-open');
   }
